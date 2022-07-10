9dfd8ccdd41bbbcc81f2d0ea0374045662e5fdea
NirajanJoc <Nirajanjoc19@gmail.com>
2022-07-10 23:56:27 +0545

    'variables'

---
+++ JavaScript/variable.js
@@ -0,0 +1,17 @@
+// define a variable 
+let name="Nirajan Joshi"
+// assign value in variable
+name='Saurav Joc'
+
+console.log(name)
+
+let age= 24
+console.log(age)
+
+let user_name="nocjoc19"
+console.log(user_name)
+
+let interest=13.5
+let principle=15000
+let amount=principle*(1+interest/100)
+console.log(amount)
