cfa3ef043d5f5e8895a0b2f72b89a470ed3dcd2c
NirajanJoc <Nirajanjoc19@gmail.com>
2022-07-12 12:43:03 +0545

    'data_type2'

---
+++ JavaScript/data_type2.js
@@ -0,0 +1,33 @@
+// String
+
+let firstname='Nirajan'
+let lastname="Joshi"
+
+let user=firstname+" "+lastname
+console.log(user)
+
+let nxtline="Nirajan \nJoshi"
+let tabin="Nirajan J\toshi"
+let backspace="Nirajann\b Joshi"
+let verticletab="N\vo\vc"
+
+console.log(verticletab)
+console.log(backspace)
+console.log(nxtline)
+console.log(tabin)
+
+// undefined
+let username
+console.log(username)
+
+// Not a Number Nan
+let data=5/firstname
+console.log(data)
+// Number type
+console.log(typeof data)
+
+// Null
+let users=null
+console.log(users)
+// Objects type
+console.log(typeof users)   
