3ef5b47b18459d6c60cf18d3687eb7921d1522a6
NirajanJoc <Nirajanjoc19@gmail.com>
2022-07-12 17:50:24 +0545

    'arithmetic_operator'

---
+++ JavaScript/arithmetic_operator.js
@@ -0,0 +1,34 @@
+let num1= 10
+let num2= 6
+console.log(num1 + num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+console.log(num1%num2)
+
+// post increment/pre increment
+let num= num1++
+console.log(num, num1)
+num=++num2
+console.log(num, num2)
+
+// shorthand operators
+let n=6
+n+=5
+console.log(n)
+n-=5
+console.log(n)
+n*=5
+console.log(n)
+n/=5
+console.log(n)
+console.log(num++)
+
+
+let numm=4
+let result=Math.pow(4, 3)
+console.log(result)
+result= numm*numm*numm
+console.log(result)
+result=numm ** 3
+console.log(result)
