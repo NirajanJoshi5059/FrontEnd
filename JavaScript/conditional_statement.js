a7c6012a045b9c7b8b410c5e04a0ff68f7607867
NirajanJoc <Nirajanjoc19@gmail.com>
2022-07-13 18:45:44 +0545

    'conditional_statement'

---
+++ JavaScript/conditional_statement.js
@@ -0,0 +1,16 @@
+let num1 =6, num2=15, num3=19
+
+if (num1%2 == 0)
+    console.log("Even Number");
+else
+    console.log("Odd Number");
+
+if(num1 > num2 && num1 > num3)
+    console.log(num1+" is greater")
+else if(num2 > num1 && num2 > num3)
+    console.log(num2+" is greater")
+else{
+    console.log(num3+" is greater.")
+    console.log("Thank You !")
+}
+console.log("Visit again")
