0559936277e1d689c2a6da22143b2fbc3acbf335
NirajanJoc <Nirajanjoc19@gmail.com>
2022-07-13 09:38:01 +0545

    'logical_operator'

---
+++ JavaScript/logical_operator.js
@@ -0,0 +1,11 @@
+// Logical Operator &&, ||, !
+let x = 5, y = 6, z = 7
+
+let result= x < y && x < z
+console.log(result)
+
+let name1 = "Nirajan", name2= "Saurav", name3 = "nirajan"
+
+result=name1 < name2 && name1 < name3
+n= !result
+console.log(result, n)
