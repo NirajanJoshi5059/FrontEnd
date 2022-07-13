2742882f2ab23f9c96eebd3afc40cc45f605598b
NirajanJoc <Nirajanjoc19@gmail.com>
2022-07-13 09:24:59 +0545

    'rational_operator

---
+++ JavaScript/rational_operator.js
@@ -0,0 +1,49 @@
+// number to number
+let num1= 5
+let num2= 7
+
+let data= num1 > num2
+console.log(data)
+
+data= num1 < num2
+console.log(data)
+num1=6
+num2=6
+data= num1 >= num2
+console.log(data)
+
+data= num1 == num2
+console.log(data)
+
+// string to string 
+let name="Nirajan"
+let name1="Saurav"
+data= name1 > name
+console.log(data)
+
+data= name1 < name
+console.log(data)
+
+let data1="Pen"
+let data2="Pencil"
+data= data1 > data2
+console.log(data)
+
+// number and string
+
+let data3="15"
+let num3= 14
+data= data3 > num3
+console.log(data)
+
+data3=""
+num3=false
+// double equal to compare value only
+data= data3 == num3
+console.log(data)
+
+// triple equal to compare value and data type 
+
+
+data= data3 === num3
+console.log(data)
