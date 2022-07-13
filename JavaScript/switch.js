ce9e2e5178b497575a32c97c694d441dd6dc860d
NirajanJoc <Nirajanjoc19@gmail.com>
2022-07-13 19:08:15 +0545

    'switch'

---
+++ JavaScript/switch.js
@@ -0,0 +1,42 @@
+let day="Friday"
+
+switch(day){
+    case "Monday":
+        console.log("7 am")
+        break;
+    case "Tuesday":
+        console.log("6:45 am")
+        break;
+    case "Wednesday":
+        console.log("6 am")
+        break;
+    case "Thursday":
+        console.log("6 am")
+        break;
+    case "Friday":
+        console.log("6:30 am")
+        break;
+    case "Saturday":
+        console.log("9 am")
+        break;
+    case "Sunday":
+        console.log("6 am")
+        break;
+}
+
+console.log("1. Momo \n2. Chowmean \n3. Pasta \n4. Thukpa")
+let menu= 3
+switch(menu){
+    case 1:
+        console.log("MO:MO ordered")
+        break
+    case 2:
+        console.log("ChowMean ordered")
+        break
+    case 3:
+        console.log("Pasta ordered")
+        break
+    case 4:
+        console.log("Thukpa ordered")
+        break
+}
