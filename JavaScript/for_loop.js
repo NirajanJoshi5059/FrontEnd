3a195bfc398247903be8b3ebfb6f4a7076ea7236
NirajanJoc <Nirajanjoc19@gmail.com>
2022-07-14 14:15:26 +0545

    'for_loop'

---
+++ JavaScript/for_loop.js
@@ -0,0 +1,12 @@
+
+for (let i=0; i<=5; i++){
+    console.log("Hi "+ i);
+}
+
+// nested for loop
+for (let i=1; i<=5; i++){
+    for(let j=0; j<=i; j++){
+        console.log("*".repeat(j));
+    }
+    console.log('\n');
+}
