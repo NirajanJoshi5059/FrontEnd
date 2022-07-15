ef1bd2a498b2ea810656764cafd1558f5642e88a
NirajanJoc <Nirajanjoc19@gmail.com>
2022-07-15 16:41:33 +0545

    'arrow_function'

---
+++ JavaScript/arrow_function.js
@@ -0,0 +1,15 @@
+// Arrow Function
+
+let greet= (user)=>{
+    console.log(`Hello ${user}`)
+}
+greet("Nirajan")
+
+
+let mul=(num1, num2)=> num1*num2;
+let result=mul(8, 99)
+console.log(result)
+
+
+let add=(num1, num2)=> console.log(num1+num2)
+add(5,9)
