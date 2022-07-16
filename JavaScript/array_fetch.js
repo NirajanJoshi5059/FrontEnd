86f50ab841b577e490f66b376a5b0673625763d3
NirajanJoc <Nirajanjoc19@gmail.com>
2022-07-16 19:30:01 +0545

    'array_define_fetch'

---
+++ JavaScript/array_fetch.js
@@ -0,0 +1,12 @@
+// first way to define array
+let val1= new Array();
+val1.push(9);
+val1.push("Nirajan")
+console.log(val1)
+
+// Second way to define array
+
+let val=[5, 7, 98,14]
+console.log(val)
+val.push("Saurav")
+console.log(val)
