50c05e733575e36dab2f8d236710a6f6f466ead6
NirajanJoc <Nirajanjoc19@gmail.com>
2022-07-16 23:13:58 +0545

    'for_of_in_array'

---
+++ JavaScript/array_forof.js
@@ -0,0 +1,12 @@
+let data=[7,14,10,9,15,17,25,26]
+
+let name=["Nischal", "Nirajan",'Saurav','Sujan','Jayanta']
+// for of loop get the value
+for(let roll of data){
+    console.log(roll)
+}
+// for in value get the key
+for(let n in name){
+    console.log(n)
+    console.log(name[n])
+}
